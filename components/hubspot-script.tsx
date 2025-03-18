b848612ad8f094d748fccaf565cb5839c9499f6d
Brad Bodine <bbodine1@gmail.com>
2025-03-18 13:16:58 -0500

    Refactor HubspotScript component for improved performance and readability
    - Updated the script import statement to use single quotes for consistency.
    - Added async and defer attributes to the Script component for better loading performance.
    - Adjusted indentation for improved code clarity.

--- components/hubspot-script.tsx
+++ components/hubspot-script.tsx
@@ -1,13 +1,14 @@
-"use client"
+'use client'
 
-import Script from "next/script"
+import Script from 'next/script'
 
 export default function HubspotScript() {
-  return (
-    <Script
-      id="hs-script-loader"
-      strategy="afterInteractive"
-      src="//js.hs-scripts.com/45346445.js"
-    />
-  )
+	return (
+		<Script
+			id="hs-script-loader"
+			async
+			defer
+			src="//js.hs-scripts.com/45346445.js"
+		/>
+	)
 }
